0f2f820f35a28548d46977f7aa99d90760f4c500
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2023-04-27 17:58:36 -0500

    added more precise pointer coordinates

--- src/assets/Models/PictureModel.jsx
+++ src/assets/Models/PictureModel.jsx
@@ -7,62 +7,148 @@ export function PictureModel() {
   const meshOne = useRef();
   const meshTwo = useRef();
   const meshThree = useRef();
-  const meshArray = [meshOne, meshTwo, meshThree];
-  const textureArray = useTexture("./justmarriedneon.jpg");
+  const meshFour = useRef();
+  const meshFive = useRef();
+  const meshSix = useRef();
+  const meshArray = [meshOne, meshTwo, meshThree, meshFour, meshFive, meshSix];
   const [prevMouseX, setPrevMouseX] = useState(0);
   const [prevMouseY, setPrevMouseY] = useState(0);
   const [meshIndex, setNextMeshIndex] = useState(0);
   const [currentMesh, setNextMesh] = useState(meshArray[meshIndex]);
+  const [currentRotationY, setNextRotationY] = useState(2);
+  const [currentRotationZ, setNextRotationZ] = useState(0.5);
 
-  useFrame((state, deltaTime) => {
-    meshOne.current.rotation.z += Math.sin(deltaTime * 0.5) * Math.PI * 0.05;
-    meshTwo.current.rotation.z += Math.sin(deltaTime * 0.5) * Math.PI * 0.05;
-    meshThree.current.rotation.z += Math.sin(deltaTime * 0.5) * Math.PI * 0.05;
+  const v = new THREE.Vector3();
 
-    meshOne.current.position.y -= 4 * deltaTime;  
-    meshTwo.current.position.y -= 4 * deltaTime;
-    meshThree.current.position.y -= 4 * deltaTime;
-  });
+  const textureArray = useTexture("./justmarriedneon.jpg");
+  textureArray.wrapS = THREE.ClampToEdgeWrapping;
+  textureArray.wrapT = THREE.ClampToEdgeWrapping;
+  textureArray.minFilter = THREE.NearestFilter;
+  textureArray.magFilter = THREE.NearestFilter;
 
-  useFrame(({ mouse, clock }) => {
-    const deltaTime = clock.getDelta();
+  useFrame((state) => {
 
-    if (
-      Math.abs(mouse.x - prevMouseX) > 0.2 ||
-      Math.abs(mouse.y - prevMouseY) > 0.2
-    ) {
-      setPrevMouseX(mouse.x);
-      setPrevMouseY(mouse.y);
+    console.log(state.pointer.x)
 
-      currentMesh.current.position.set(mouse.x * 10, mouse.y * 5, 0);
+    v.copy({ x: state.pointer.x, y: state.pointer.y, z: 0 });
+    v.unproject(state.camera);
+    
 
-      if(meshIndex === meshArray.length - 1){
-        setNextMeshIndex(0)
-      }else{
-        setNextMeshIndex(meshIndex + 1)
+
+    if(Math.abs(state.pointer.x - prevMouseX) > 0.2 ){
+      currentMesh.current.position.copy({x: v.x, y: v.y, z: v.z})
+
+
+      // currentMesh.current.position.lerp({ x: v.x, y: v.y, z: v.z }, .1)
+      // currentMesh.current.rotation.z += Math.PI * (v.x * .1)
+  
+      if (meshIndex === meshArray.length - 1) {
+        setNextMeshIndex(0);
+      } else {
+        setNextMeshIndex(meshIndex + 1);
       }
-      
-      setNextMesh(meshArray[meshIndex])
-
-
+  
+      setNextMesh(meshArray[meshIndex]);
+      setPrevMouseX(state.pointer.x)
     }
   });
+
+
+  useFrame((state) => {
+    v.copy({ x:state.pointer.x, y:state.pointer.y, z:0})
+    v.unproject(state.camera)
+
+    // meshOne.current.position.lerp({x: v.x, y: v.y, z: v.z}, 0.001)
+    // meshTwo.current.position.lerp({x: v.x, y: v.y, z: v.z}, 0.01)
+    // meshThree.current.position.lerp({x: v.x, y: v.y, z: v.z}, 0.01)
+    // meshFour.current.position.lerp({x: v.x, y: v.y, z: v.z}, 0.01)
+    // meshFive.current.position.lerp({x: v.x, y: v.y, z: v.z}, 0.01)
+    // meshSix.current.position.lerp({x: v.x, y: v.y, z: v.z}, 0.01)
+
+
+    meshOne.current.position.y -= .1
+    meshTwo.current.position.y  -= .1
+    meshThree.current.position.y  -= .1
+    meshFour.current.position.y  -= .1
+    meshFive.current.position.y  -= .1
+    meshSix.current.position.y  -= .1 
+  })
+
+  // useFrame(({ mouse, clock }, deltaTime) => {
+  //   // meshOne.current.rotation.z += Math.sin(deltaTime * currentRotationZ) * Math.PI * 0.005;
+  //   meshTwo.current.rotation.z += Math.sin(deltaTime * currentRotationZ) * Math.PI * 0.005;
+  //   meshThree.current.rotation.z += Math.sin(deltaTime * currentRotationZ) * Math.PI * 0.005;
+  //   meshFour.current.rotation.z += Math.sin(deltaTime * currentRotationZ) * Math.PI * 0.005;
+  //   meshFive.current.rotation.z += Math.sin(deltaTime * currentRotationZ) * Math.PI * 0.005;
+  //   meshSix.current.rotation.z += Math.sin(deltaTime * currentRotationZ) * Math.PI * 0.005;
+
+  //   // meshOne.current.position.y -= currentRotationY * deltaTime;
+  //   meshTwo.current.position.y -= currentRotationY * deltaTime;
+  //   meshThree.current.position.y -= currentRotationY * deltaTime;
+  //   meshFour.current.position.y -= currentRotationY * deltaTime;
+  //   meshFive.current.position.y -= currentRotationY * deltaTime;
+  //   meshSix.current.position.y -= currentRotationY * deltaTime;
+
+  //   if (
+  //     Math.abs(mouse.x - prevMouseX) > 0.1 ||
+  //     Math.abs(mouse.y - prevMouseY) > 0.1
+  //   ) {
+
+  //     if(mouse.y > prevMouseY){
+  //       setNextRotationY(-4)
+  //     }else{
+  //       setNextRotationY(4)
+  //     }
+
+  //     if(mouse.x > prevMouseX){
+  //       setNextRotationZ(-4)
+  //     }else{
+  //       setNextRotationZ(4)
+  //     }
+
+  //     currentMesh.current.position.set(mouse.x * 10, mouse.y * 5, 0);
+
+  //     if (meshIndex === meshArray.length - 1) {
+  //       setNextMeshIndex(0);
+  //     } else {
+  //       setNextMeshIndex(meshIndex + 1);
+  //     }
+
+  //     setNextMesh(meshArray[meshIndex]);
+  //     setPrevMouseX(mouse.x);
+  //     setPrevMouseY(mouse.y);
+  //   }
+  // });
   return (
     <>
-
       <mesh ref={meshOne} position={[0, 10, 0]}>
-        <planeGeometry args={[5, 5]} />
-        <meshStandardMaterial color={"blue"} />
+        <planeGeometry args={[2.5, 3.25]} />
+        <meshStandardMaterial map={textureArray} />
       </mesh>
 
       <mesh ref={meshTwo} position={[0, 10, 0]}>
-        <planeGeometry args={[5, 5]} />
+        <planeGeometry args={[2.5, 3.25]} />
         <meshStandardMaterial color={"green"} />
       </mesh>
 
       <mesh ref={meshThree} position={[0, 10, 0]}>
-        <planeGeometry args={[5, 5]} />
+        <planeGeometry args={[2.5, 3.25]} />
         <meshStandardMaterial color={"purple"} />
+      </mesh>
+
+      <mesh ref={meshFour} position={[0, 10, 0]}>
+        <planeGeometry args={[2.5, 3.25]} />
+        <meshStandardMaterial color={"red"} />
+      </mesh>
+
+      <mesh ref={meshFive} position={[0, 10, 0]}>
+        <planeGeometry args={[2.5, 3.25]} />
+        <meshStandardMaterial color={"yellow"} />
+      </mesh>
+
+      <mesh ref={meshSix} position={[0, 10, 0]}>
+        <planeGeometry args={[2.5, 3.25]} />
+        <meshStandardMaterial color={"white"} />
       </mesh>
     </>
   );
